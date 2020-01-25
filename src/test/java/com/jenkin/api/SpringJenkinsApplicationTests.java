8789951ac4ad966a39df508bcff1fc1ce162da64
Srikanth Dannarapu <srikant.dannarapu@gmail.com>
2020-01-25 15:54:35 +0530

    fail commit

--- src/test/java/com/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkin/api/SpringJenkinsApplicationTests.java
@@ -16,7 +16,7 @@ public class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	public void contextLoads() {
+	public contextLoads() {
 		logger.info("*** Test case executing...");
 		logger.info("*** log updated in second commit...");
 		assertEquals(true, true);
